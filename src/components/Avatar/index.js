604030f7c7a4b208daca664d657074aa78d835d5
Junior Alves <jrnalves@gmail.com>
2021-05-26 01:25:33 -0300

    create avatar components

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,8 @@
+import avatar from "../../assets/buck.jpg";
+import "./styles.css";
+
+function Avatar() {
+  return <img src={avatar} className="avatar" alt="avatar buck" />;
+}
+
+export default Avatar;
